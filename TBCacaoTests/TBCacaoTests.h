46876c1cac604c7018c2e0c7f4987f3804c75b0a
qvacua <qvacua@gmail.com>
2011-04-07 22:22:24 +0200

    Added initial Xcode project

---
+++ TBCacaoTests/TBCacaoTests.h
@@ -0,0 +1,17 @@
+//
+//  TBCacaoTests.h
+//  TBCacaoTests
+//
+//  Created by Tae on 4/7/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface TBCacaoTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ TBCacaoTests/TBCacaoTests.m
@@ -0,0 +1,33 @@
+//
+//  TBCacaoTests.m
+//  TBCacaoTests
+//
+//  Created by Tae on 4/7/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "TBCacaoTests.h"
+
+
+@implementation TBCacaoTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TBCacaoTests");
+}
+
+@end
