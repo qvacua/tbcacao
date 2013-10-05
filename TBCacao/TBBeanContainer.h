b4e87d6b301ebe14adc2857f88496fbfc2907ea8
Tae Won Ha <qvacua@gmail.com>
2013-10-05 21:18:56 +0200

    add scope to bean container

--- TBCacao/TBBeanContainer.h
+++ TBCacao/TBBeanContainer.h
@@ -8,25 +8,50 @@
 
 #import <Foundation/Foundation.h>
 
+typedef enum {
+    TBBeanScopeSingleton = 0,
+    TBBeanScopePrototype,
+} TBBeanScope;
+
 @interface TBBeanContainer : NSObject
 
 @property (readonly) NSString *identifier;
 @property (readonly) id targetSource;
+@property (readonly) TBBeanScope scope;
 
+/**
+* Initializes the bean container with the scope TBBeanScopeSingleton
+*/
 - (id)initWithIdentifier:(NSString *)anIdentifier bean:(id)aBean;
 
 /**
-* The identifier will be bean's class name, i.e. NSStringFromClass([targetSource class])
+* Designated initializer
+*/
+- (id)initWithIdentifier:(NSString *)anIdentifier bean:(id)aBean scope:(TBBeanScope)aScope;
+
+/**
+* The identifier will be bean's class name and the scope TBBeanScopeSingleton
 */
 - (id)initWithBean:(id)bean;
 
 /**
-* The identifier will be bean's class name, i.e. NSStringFromClass([targetSource class])
+* The identifier will be bean's class name and the scope TBBeanScopeSingleton
 */
 + (id)beanContainerWithBean:(id)targetSource;
+
+/**
+* The scope will be TBBeanScopeSingleton
+*/
 + (id)beanContainerWithIdentifier:(NSString *)anIdentifier bean:(id)aBean;
 
-- (BOOL)isEqual:(TBBeanContainer *)beanContainerToCompare;
+/**
+* Designated initializer
+*/
++ (id)beanContainerWithIdentifier:(NSString *)anIdentifier bean:(id)aBean scope:(TBBeanScope)aScope;
+
+- (BOOL)isEqual:(TBBeanContainer *)other;
+- (BOOL)isEqualToContainer:(TBBeanContainer *)container;
+- (NSUInteger)hash;
 - (NSString *)description;
 
 @end
--- TBCacao/TBBeanContainer.m
+++ TBCacao/TBBeanContainer.m
@@ -7,29 +7,29 @@
  */
 
 #import "TBBeanContainer.h"
+#import "NSObject+TBCacao.h"
 
-@implementation TBBeanContainer {
-@private
-    NSString *_identifier;
-    id _targetSource;
+
+@implementation TBBeanContainer
+
+#pragma mark Public
+- (id)initWithBean:(id)bean {
+    return [self initWithIdentifier:[bean classAsString] bean:bean];
 }
 
-@synthesize identifier = _identifier;
-@synthesize targetSource = _targetSource;
-
-#pragma mark NSObject
 - (id)initWithIdentifier:(NSString *)anIdentifier bean:(id)aBean {
+    return [self initWithIdentifier:anIdentifier bean:aBean scope:TBBeanScopeSingleton];
+}
+
+- (id)initWithIdentifier:(NSString *)anIdentifier bean:(id)aBean scope:(TBBeanScope)aScope {
     self = [super init];
     if (self) {
         _identifier = anIdentifier;
         _targetSource = aBean;
+        _scope = aScope;
     }
 
     return self;
-}
-
-- (id)initWithBean:(id)bean {
-    return [self initWithIdentifier:NSStringFromClass([bean class]) bean:bean];
 }
 
 + (id)beanContainerWithBean:(id)targetSource {
@@ -40,27 +40,49 @@
     return [[TBBeanContainer alloc] initWithIdentifier:anIdentifier bean:aBean];
 }
 
++ (id)beanContainerWithIdentifier:(NSString *)anIdentifier bean:(id)aBean scope:(TBBeanScope)aScope {
+    return [[TBBeanContainer alloc] initWithIdentifier:anIdentifier bean:aBean scope:aScope];
+}
+
+#pragma mark NSObject
 - (BOOL)isEqual:(TBBeanContainer *)other {
-    if (other == self) {
+    if (other == self)
         return YES;
-    }
-
-    if (!other || ![other isKindOfClass:[self class]]) {
+    if (!other || ![[other class] isEqual:[self class]])
         return NO;
-    }
 
-    return [self.identifier isEqualToString:other.identifier];
+    return [self isEqualToContainer:other];
+}
+
+- (BOOL)isEqualToContainer:(TBBeanContainer *)container {
+    if (self == container)
+        return YES;
+    if (container == nil)
+        return NO;
+    if (self.identifier != container.identifier && ![self.identifier isEqualToString:container.identifier])
+        return NO;
+    if (self.targetSource != container.targetSource && ![self.targetSource isEqual:container.targetSource])
+        return NO;
+    if (self.scope != container.scope)
+        return NO;
+    return YES;
 }
 
 - (NSUInteger)hash {
     NSUInteger hash = [self.identifier hash];
     hash = hash * 31u + [self.targetSource hash];
-
+    hash = hash * 31u + (NSUInteger) self.scope;
     return hash;
 }
 
 - (NSString *)description {
-    return [[NSString alloc] initWithFormat:@"Bean %@ of class %@", self.identifier, NSStringFromClass([self.targetSource class])];
+    NSMutableString *description = [NSMutableString stringWithFormat:@"<%@: ", NSStringFromClass([self class])];
+    [description appendFormat:@"self.identifier=%@", self.identifier];
+    [description appendFormat:@", self.targetSource=%@", self.targetSource];
+    [description appendFormat:@", self.scope=%d", self.scope];
+    [description appendString:@">"];
+
+    return description;
 }
 
 @end
