3e6e3ec31d77951d3080ee6f4d2ca90168bba70a
Tae Won Ha <qvacua@me.com>
2014-12-20 20:51:51 +0100

    reformat

--- TBCacao/CocoaCategories/NSString+TBCacao.h
+++ TBCacao/CocoaCategories/NSString+TBCacao.h
@@ -1,18 +1,19 @@
 /**
- * Tae Won Ha
- * http://qvacua.com
- * https://github.com/qvacua
- *
- * See LICENSE
- */
+* Tae Won Ha
+* http://qvacua.com
+* https://github.com/qvacua
+*
+* See LICENSE
+*/
 
 #import <Foundation/Foundation.h>
+
 
 @interface NSString (TBCacao)
 
 /**
- * Returns YES if the string starts with str.
- */
+* Returns YES if the string starts with str.
+*/
 - (BOOL)startsWithString:(NSString *)str;
 
 @end
--- TBCacao/CocoaCategories/NSString+TBCacao.m
+++ TBCacao/CocoaCategories/NSString+TBCacao.m
@@ -1,20 +1,21 @@
 /**
- * Tae Won Ha
- * http://qvacua.com
- * https://github.com/qvacua
- *
- * See LICENSE
- */
+* Tae Won Ha
+* http://qvacua.com
+* https://github.com/qvacua
+*
+* See LICENSE
+*/
 
 #import "NSString+TBCacao.h"
+
 
 @implementation NSString (TBCacao)
 
 - (BOOL)startsWithString:(NSString *)str {
-    NSUInteger length = [str length];
-    NSComparisonResult result = [self compare:str options:NSLiteralSearch range:NSMakeRange(0, length)];
-    
-    return result == NSOrderedSame;
+  NSUInteger length = [str length];
+  NSComparisonResult result = [self compare:str options:NSLiteralSearch range:NSMakeRange(0, length)];
+
+  return result == NSOrderedSame;
 }
 
 @end
